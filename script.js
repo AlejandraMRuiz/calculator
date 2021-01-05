baec1383a10accfeb9a0d60b0e470e23db461572
AlejandraMRuiz <kovydcode@gmail.com>
2021-01-04 22:37:59 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+// PROJECT PLAN:
+
+// STEP 1: Add two numbers
+// in progresss...
+
+
+// STEP 2: Subtract two numbers
+
+// STEP 3: Multiply two numbers
+
+// STEP 4: Divide two numbers
+
+// STEP 5: Display at least 10 digits on the screen
+
+// STEP 6: Display decimals to at least the thousandth (.001)
+
+// STEP 7: 
+// Perform continuous functions
+// Example -
+// Press the 1 button
+// Press the add button
+// Press the 1 button
+// Press the add button again (result should show as 2 on the screen)
+// Press the 1 button
+// Press the add button again (result should show as 3 on the screen)
+// Pressing the clear button resets screen to 0
